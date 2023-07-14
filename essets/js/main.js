4f83ddde56867e0746d253643b882509d1afc875
Mahamed Bedier <113152094+mahamedbedier@users.noreply.github.com>
2023-07-14 22:50:57 +0300

    Add files via upload

---
+++ essets/js/main.js
@@ -0,0 +1,25 @@
+let humburger = document.getElementById("humburger")
+let navBar = document.getElementById("nav")
+let homeNav = document.getElementById("home-nav")
+let aboutmenav = document.getElementById("about-me-nav")
+let projectsnav = document.getElementById("projects-nav")
+let myskillsnav = document.getElementById("my-skills-nav")
+let contctnav = document.getElementById("contct-nav")
+humburger.onclick = function () {
+    navBar.classList.toggle("active")
+}
+homeNav.onclick = function () {
+    navBar.classList.toggle("active")
+}
+aboutmenav.onclick = function () {
+    navBar.classList.toggle("active")
+}
+projectsnav.onclick = function () {
+    navBar.classList.toggle("active")
+}
+myskillsnav.onclick = function () {
+    navBar.classList.toggle("active")
+}
+contctnav.onclick = function () {
+    navBar.classList.toggle("active")
+}
